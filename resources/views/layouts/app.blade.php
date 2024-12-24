b234b9ef19028993e98b1c6f71c76e4ec8e76a34
Angus <angus.goody@gmail.com>
2024-12-24 15:41:35 +0000

    colour tweaks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-base-100">
+        <div class="min-h-screen bg-base-200">
             <livewire:layout.navigation />
 
             <!-- Page Heading -->
